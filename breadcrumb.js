4e764953892976ff8b95a3fba18a5b9b2d4a24bb
zhanik228 <zhan142004@gmail.com>
2024-03-18 17:08:43 +0500

    fixed navigation

--- breadcrumb.js
+++ breadcrumb.js
@@ -42,3 +42,20 @@ document.addEventListener("DOMContentLoaded", function () {
     swiper.update(); // Update swiper instance after changing the configuration
   });
 });
+
+const tabsBox = document.querySelector('.breadcrumb--sm')
+
+let isDragging = false
+
+const dragging = (e) => {
+  if (!isDragging) return
+  tabsBox.scrollLeft -= e.movementX
+}
+
+const dragStop = () => {
+  isDragging = false
+}
+
+tabsBox.addEventListener('mousedown', () => isDragging = true)
+tabsBox.addEventListener('mousemove', dragging)
+document.addEventListener('mouseup', dragStop)
--- profile.js
+++ profile.js
@@ -43,4 +43,22 @@ document.addEventListener("DOMContentLoaded", function () {
       swiper.update(); // Update swiper instance after changing the configuration
     });
   });
+
+
+  const tabsBox = document.querySelector('.profile-nav__list')
+
+  let isDragging = false
+  
+  const dragging = (e) => {
+    if (!isDragging) return
+    tabsBox.scrollLeft -= e.movementX
+  }
+  
+  const dragStop = () => {
+    isDragging = false
+  }
+  
+  tabsBox.addEventListener('mousedown', () => isDragging = true)
+  tabsBox.addEventListener('mousemove', dragging)
+  document.addEventListener('mouseup', dragStop)
   
